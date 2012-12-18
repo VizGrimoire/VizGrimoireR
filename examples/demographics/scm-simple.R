831c8744e6228e148f7d9e107782f1eafe982d5a
Alvaro del Castillo <acs@barrapunto.com>
2012-12-18 08:17:56 +0100

    Use a better name for the demographics file: scm-demographics-2012.json

--- examples/demographics/scm-simple.R
+++ examples/demographics/scm-simple.R
@@ -1,10 +1,10 @@
 library("vizgrimoire")
 
 ## Analyze command line args, and produce config params from them
-#conf <- ConfFromParameters("kdevelop", "jgb", "XXX")
-conf <- ConfFromParameters(dbschema = "acs_cvsanaly_allura", group = "fuego")
-#SetDBChannel (conf$user, conf$password, conf$database)
-SetDBChannel (database = conf$database, group = conf$group)
+conf <- ConfFromParameters("acs_cvsanaly_allura", "allura", "allura")
+# conf <- ConfFromParameters(dbschema = "acs_cvsanaly_allura", group = "fuego")
+SetDBChannel (conf$user, conf$password, conf$database)
+# SetDBChannel (database = conf$database, group = conf$group)
 
 demos <- new ("Demographics")
 
@@ -12,3 +12,4 @@ Pyramid (demos, "2010-01-01", "/tmp/demos-pyramid-2010")
 JSON (demos, "/tmp/demos-pyramid-2010.json")
 Pyramid (demos, "2012-01-01", "/tmp/demos-pyramid-2012")
 JSON (demos, "/tmp/demos-pyramid-2012.json")
+JSON (demos, "/tmp/scm-demographics-2012.json")
