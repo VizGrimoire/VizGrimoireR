5454f1b883536a4c36455c2700eb35eb73093c6b
Jesus M. Gonzalez-Barahona <jgb@gsyc.es>
2012-12-26 10:59:21 +0100

    Adapting to changes (to work with 127.0.0.1)

--- examples/demographics/scm-demographics.R
+++ examples/demographics/scm-demographics.R
@@ -31,8 +31,14 @@ library("vizgrimoire")
 ## Analyze args, and produce config params from them
 #conf <- ConfFromParameters("kdevelop", "jgb", "XXX")
 #SetDBChannel (conf$user, conf$password, conf$database)
-conf <- ConfFromParameters(dbschema = "dic_cvsanaly_linux_git", group = "fuego")
-SetDBChannel (database = conf$database, group = conf$group)
+conf <- ConfFromParameters(dbschema = "dic_cvsanaly_linux_git",
+                           user = "root", password = NULL,
+                           host = "127.0.0.1", port = 3308)
+SetDBChannel (database = conf$database,
+              user = conf$user, password = conf$password,
+              host = conf$host, port = conf$port)
+#conf <- ConfFromParameters(dbschema = "dic_cvsanaly_linux_git", group = "fuego")
+#SetDBChannel (database = conf$database, group = conf$group)
 
 demos <- new ("Demographics")
 ages <- GetAges (demos, "2012-10-01")
