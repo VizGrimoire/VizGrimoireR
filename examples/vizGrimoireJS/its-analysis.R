f4942ff726be487e4ec26da0df67a1e0ccde02de
Alvaro del Castillo <acs@bitergia.com>
2013-03-06 11:29:13 +0100

    Fixed the closed condition for JIRA

--- examples/vizGrimoireJS/its-analysis.R
+++ examples/vizGrimoireJS/its-analysis.R
@@ -46,7 +46,7 @@ if (conf$backend == 'bugzilla')
 	closed_condition <- "(new_value='RESOLVED' OR new_value='CLOSED')"
 if (conf$backend == 'github') closed_condition <- "field='closed'"
 if (conf$backend == 'jira') 
-	closed_condition <- "(new_value='RESOLVED' OR new_value='CLOSED')"
+	closed_condition <- "new_value='CLOSED'"
 
 closed_monthly <- evol_closed(closed_condition)
 changed_monthly <- evol_changed()
