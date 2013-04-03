5a01ea2ad8022f919eb68faf78c57a227b987f23
Alvaro del Castillo <acs@bitergia.com>
2013-04-03 19:42:51 +0200

    Added its type and create dir structure

--- examples/vizGrimoireJS/run_scripts.sh
+++ examples/vizGrimoireJS/run_scripts.sh
@@ -3,8 +3,11 @@
 #$2 = MLS database
 #$3 = ITS database
 
+# Example: ./run_scripts.sh dic_cvsanaly_openstack_1289_2013_04_02 dic_mlstats_openstack_1290_2013_04_02 lcanas_bicho_openstack_1291_2013_04_02
+
 #WHOLE DATA
 rm data/json/*
+mkdir -p data/whole_project
 #SCM
 R --vanilla --args -d $1 -u root -i $1 -r repositories -g weeks < scm-analysis.R
 sleep 5
@@ -13,13 +16,14 @@ R --vanilla --args -d $1 -u root -i $1 -r companies -g weeks < scm-analysis.R
 R --vanilla --args -d $2 -u root -i $1 -r repositories -g weeks < mls-analysis.R
 R --vanilla --args -d $2 -u root -i $1 -r companies -g weeks < mls-analysis.R
 #ITS
-R --vanilla --args -d $3 -u root -i $1 -r repositories -g weeks < its-analysis.R
-R --vanilla --args -d $3 -u root -i $1 -r companies -g weeks < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r repositories -g weeks -t launchpad < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r companies -g weeks -t launchpad < its-analysis.R
 rm data/whole_project/*
 mv data/json/* data/whole_project/
 
 #GRIZZLY
 rm data/json/*
+mkdir -p data/grizzly
 #SCM
 R --vanilla --args -d $1 -u root -i $1 -r repositories -s 2012-09-27 -g weeks < scm-analysis.R
 R --vanilla --args -d $1 -u root -i $1 -r companies -s 2012-09-27 -g weeks < scm-analysis.R
@@ -27,13 +31,14 @@ R --vanilla --args -d $1 -u root -i $1 -r companies -s 2012-09-27 -g weeks < scm
 R --vanilla --args -d $2 -u root -i $1 -r repositories -s 2012-09-27 -g weeks < mls-analysis.R
 R --vanilla --args -d $2 -u root -i $1 -r companies -s 2012-09-27 -g weeks < mls-analysis.R
 #ITS
-R --vanilla --args -d $3 -u root -i $1 -r repositories -s 2012-09-27 -g weeks < its-analysis.R
-R --vanilla --args -d $3 -u root -i $1 -r companies -s 2012-09-27 -g weeks < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r repositories -s 2012-09-27 -g weeks -t launchpad < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r companies -s 2012-09-27 -g weeks -t launchpad < its-analysis.R
 rm data/grizzly/*
 mv data/json/* data/grizzly/
 
 #FOLSOM
 rm data/json/*
+mkdir -p data/folsom
 #SCM
 R --vanilla --args -d $1 -u root -i $1 -r repositories -s 2012-04-05 -e 2012-09-27 -g weeks < scm-analysis.R
 R --vanilla --args -d $1 -u root -i $1 -r companies -s 2012-04-05 -e 2012-09-27 -g weeks < scm-analysis.R
@@ -41,13 +46,14 @@ R --vanilla --args -d $1 -u root -i $1 -r companies -s 2012-04-05 -e 2012-09-27 
 R --vanilla --args -d $2 -u root -i $1 -r repositories -s 2012-04-05 -e 2012-09-27 -g weeks < mls-analysis.R
 R --vanilla --args -d $2 -u root -i $1 -r companies -s 2012-04-05 -e 2012-09-27 -g weeks < mls-analysis.R
 #ITS
-R --vanilla --args -d $3 -u root -i $1 -r repositories -s 2012-04-05 -e 2012-09-27 -g weeks < its-analysis.R
-R --vanilla --args -d $3 -u root -i $1 -r companies -s 2012-04-05 -e 2012-09-27 -g weeks < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r repositories -s 2012-04-05 -e 2012-09-27 -g weeks -t launchpad < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r companies -s 2012-04-05 -e 2012-09-27 -g weeks -t launchpad < its-analysis.R
 rm data/folsom/*
 mv data/json/* data/folsom/
 
 #ESSEX
 rm data/json/*
+mkdir -p data/essex
 #SCM
 R --vanilla --args -d $1 -u root -i $1 -r repositories -s 2011-11-22 -e 2012-04-05 -g weeks < scm-analysis.R
 R --vanilla --args -d $1 -u root -i $1 -r companies -s 2011-11-22 -e 2012-04-05 -g weeks < scm-analysis.R
@@ -55,8 +61,8 @@ R --vanilla --args -d $1 -u root -i $1 -r companies -s 2011-11-22 -e 2012-04-05 
 R --vanilla --args -d $2 -u root -i $1 -r repositories -s 2011-11-22 -e 2012-04-05 -g weeks < mls-analysis.R
 R --vanilla --args -d $2 -u root -i $1 -r companies -s 2011-11-22 -e 2012-04-05 -g weeks < mls-analysis.R
 #ITS
-R --vanilla --args -d $3 -u root -i $1 -r repositories -s 2011-11-22 -e 2012-04-05 -g weeks < its-analysis.R
-R --vanilla --args -d $3 -u root -i $1 -r companies -s 2011-11-22 -e 2012-04-05 -g weeks < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r repositories -s 2011-11-22 -e 2012-04-05 -g weeks -t launchpad < its-analysis.R
+R --vanilla --args -d $3 -u root -i $1 -r companies -s 2011-11-22 -e 2012-04-05 -g weeks -t launchpad < its-analysis.R
 rm data/essex/*
 mv data/json/* data/essex/
 
