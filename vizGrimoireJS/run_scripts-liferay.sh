f8a46320faf120d639c4d912fa5ba292496489d4
Luis Cañas-Díaz <lcanas@bitergia.com>
2013-04-23 09:20:22 +0200

    Update comment about databases in Liferay script

--- vizGrimoireJS/run_scripts-liferay.sh
+++ vizGrimoireJS/run_scripts-liferay.sh
@@ -3,7 +3,7 @@
 #$2 = MLS database
 #$3 = ITS database
 
-# Liferay: ./run_scripts-liferay.sh acs_cvsanaly_liferay_1248 acs_mlstats_liferay_forums_1294 lcanas_bicho_liferay_1250
+# Liferay: ./run_scripts-liferay.sh lcanas_cvsanaly_liferay_1423 acs_mlstats_liferay_forums_1294 lcanas_bicho_liferay_1250
 
 START=2004-08-05
 END=2013-03-01
@@ -17,18 +17,18 @@ mkdir -p data/$PROJECT
 echo "Analisys from $START to $END"
 echo "LOGS in $LOGS"
 echo "In SCM Analysis ..."
-R --vanilla --args -d $1 -u root -i $1 -r repositories -s $START -e $END -g months < scm-analysis.R >> $LOGS 2>&1
-R --vanilla --args -d $1 -u root -i $1 -r companies -s $START -e $END -g months < scm-analysis.R >> $LOGS 2>&1
-R --vanilla --args -d $1 -u root -i $1 -r countries -s $START -e $END -g months < scm-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $1 -u root -p root -i $1 -r repositories -s $START -e $END -g months < scm-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $1 -u root -p root -i $1 -r companies -s $START -e $END -g months < scm-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $1 -u root -p root -i $1 -r countries -s $START -e $END -g months < scm-analysis.R >> $LOGS 2>&1
 #ITS
 echo "In ITS Analysis ..."
-R --vanilla --args -d $3 -u root -i $1 -r repositories -s $START -e $END -g months -t allura < its-analysis.R >> $LOGS 2>&1
-R --vanilla --args -d $3 -u root -i $1 -r companies -s $START -e $END -g months -t allura < its-analysis.R >> $LOGS 2>&1
-R --vanilla --args -d $3 -u root -i $1 -r countries -s $START -e $END -g months -t allura < its-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $3 -u root -p root -i $1 -r repositories -s $START -e $END -g months -t allura < its-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $3 -u root -p root -i $1 -r companies -s $START -e $END -g months -t allura < its-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $3 -u root -p root -i $1 -r countries -s $START -e $END -g months -t allura < its-analysis.R >> $LOGS 2>&1
 #MLS
 echo "In MLS Analysis ..."
-R --vanilla --args -d $2 -u root -i $1 -r repositories -s $START -e $END -g months < mls-analysis.R >> $LOGS 2>&1
-R --vanilla --args -d $2 -u root -i $1 -r companies -s $START -e $END -g months < mls-analysis.R >> $LOGS 2>&1
-R --vanilla --args -d $2 -u root -i $1 -r countries -s $START -e $END -g months < mls-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $2 -u root -p root -i $1 -r repositories -s $START -e $END -g months < mls-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $2 -u root -p root -i $1 -r companies -s $START -e $END -g months < mls-analysis.R >> $LOGS 2>&1
+R --vanilla --args -d $2 -u root -p root -i $1 -r countries -s $START -e $END -g months < mls-analysis.R >> $LOGS 2>&1
 rm data/$PROJECT/*
 mv data/json/* data/$PROJECT/
