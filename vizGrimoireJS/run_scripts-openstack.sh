7928e558ea5d3d439bfe613046fcc42cfb3770cf
Daniel Izquierdo <dizquierdo@bitergia.com>
2013-05-17 18:40:47 +0200

    added options for START and END date

--- vizGrimoireJS/run_scripts-openstack.sh
+++ vizGrimoireJS/run_scripts-openstack.sh
@@ -2,6 +2,18 @@
 #$1 = SCM database
 #$2 = MLS database
 #$3 = ITS database
+#$4 = end date
+#$5 = destination
+#$6 = log file
+
+
+START=2010-05-27
+END=$4
+PROJECT=openstack
+DIR=$5 #not used so far
+LOGS=$6
+
+
 
 # Example: ./run_scripts-openstack.sh dic_cvsanaly_openstack_1289_2013_04_04 dic_mlstats_openstack_1290_2013_04_04 lcanas_bicho_openstack_1291_2013_04_04
 
@@ -9,11 +21,11 @@
 rm data/json/*
 mkdir -p data/whole_project
 #SCM
-LANG= R_LIBS=../r-lib:$R_LIBS R --vanilla --args -d $1 -u root -i $1 -r repositories,companies -s 2010-05-27 -e 2013-04-04 -g weeks < scm-analysis.R
+LANG= R_LIBS=../r-lib:$R_LIBS R --vanilla --args -d $1 -u root -i $1 -r repositories,companies -s $START -e $END -g weeks < scm-analysis.R
 #MLS
-LANG= R_LIBS=../r-lib:$R_LIBS R --vanilla --args -d $2 -u root -i $1 -r repositories,companies -s 2010-05-27 -e 2013-04-04 -g weeks < mls-analysis.R
+LANG= R_LIBS=../r-lib:$R_LIBS R --vanilla --args -d $2 -u root -i $1 -r repositories,companies -s $START -e $END -g weeks < mls-analysis.R
 #ITS
-LANG= R_LIBS=../r-lib:$R_LIBS R --vanilla --args -d $3 -u root -i $1 -r repositories,companies -s 2010-05-27 -e 2013-04-04 -g weeks -t launchpad < its-analysis.R
+LANG= R_LIBS=../r-lib:$R_LIBS R --vanilla --args -d $3 -u root -i $1 -r repositories,companies -s $START -e $END -g weeks -t launchpad < its-analysis.R
 rm data/whole_project/*
 mv data/json/* data/whole_project/
 
