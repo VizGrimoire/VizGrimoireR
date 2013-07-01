f5dc6f7b7244cb7e0e2ec0c14cf07be22988ac2d
Alvaro del Castillo <acs@bitergia.com>
2013-07-01 11:12:43 +0200

    Added new last activity data to compute tendencies in JS

--- vizGrimoireJS/scm-analysis.R
+++ vizGrimoireJS/scm-analysis.R
@@ -89,9 +89,13 @@ createJSON (evol_data, paste(destdir,"/scm-evolutionary.json", sep=''))
 static_data = GetSCMStaticData(period, conf$startdate, conf$enddate, conf$identities_db)
 static_url <- StaticURL()
 latest_activity7 = last_activity(7)
+latest_activity14 = last_activity(14)
 latest_activity30 = last_activity(30)
+latest_activity60 = last_activity(60)
 latest_activity90 = last_activity(90)
+latest_activity180 = last_activity(180)
 latest_activity365 = last_activity(365)
+latest_activity730 = last_activity(730)
 
 #Data for specific analysis
 if ('companies' %in% reports){
@@ -105,9 +109,13 @@ if ('countries' %in% reports){
 # 2- Merging information
 static_data = merge(static_data, static_url)
 static_data = merge(static_data, latest_activity7)
+static_data = merge(static_data, latest_activity14)
 static_data = merge(static_data, latest_activity30)
+static_data = merge(static_data, latest_activity60)
 static_data = merge(static_data, latest_activity90)
+static_data = merge(static_data, latest_activity180)
 static_data = merge(static_data, latest_activity365)
+static_data = merge(static_data, latest_activity730)
 
 
 # 3- Creating file with static data
