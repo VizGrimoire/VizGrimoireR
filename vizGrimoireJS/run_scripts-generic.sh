98f645ddfbc92bc84c9311b0a1e041d32a2ae239
Alvaro del Castillo <acs@bitergia.com>
2013-08-07 18:14:18 +0200

    Added generic script. I hope we can share it in the future.

---
+++ vizGrimoireJS/run_scripts-generic.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#$1 = SCM database
+#$2 = MLS database
+#$3 = ITS database
+#$4 = end date
+#$5 = destination
+#$6 = SCR database
+#$7 = IRC database
+
+# ./run_scripts-mediawiki.sh acs_cvsanaly_mediawiki_1571 acs_mlstats_mediawiki_1466 acs_bicho_mediawiki_1466 2013-06-01 /tmp
+
+START=2009-10-14
+#END=2013-03-01
+END=$4
+LOGS=generic.log
+DIR=$5
+# REPORTS="repositories,countries,companies,people"
+REPORTS="repositories"
+
+echo "Analisys from $START to $END"
+echo "LOGS in $LOGS"
+#MLS
+echo "In MLS Analysis ..."
+LANG= R_LIBS=../../r-lib:$R_LIBS R --vanilla --args -r $REPORTS -d $2 -u root -i $1 -s $START -e $END -o $DIR -g months < mls-analysis.R >> $LOGS 2>&1
+
+#SCM
+echo "In SCM Analysis ..."
+LANG= R_LIBS=../../r-lib:$R_LIBS R --vanilla --args -r $REPORTS -d $1 -u root -i $1 -s $START -e $END -o $DIR -g months < scm-analysis.R >> $LOGS 2>&1
+
+#ITS
+echo "In ITS Analysis ..."
+LANG= R_LIBS=../../r-lib:$R_LIBS R --vanilla --args -r $REPORTS -d $3 -u root -i $1 -s $START -e $END -o $DIR -g months -t bugzilla < its-analysis.R >> $LOGS 2>&1
+
+# SCR: repositories not working yet
+echo "In SCR Analysis ..."
+LANG= R_LIBS=../../r-lib:$R_LIBS R --vanilla --args -r none -d $6 -u root -i $1  -s $START -e $END -o $DIR -g months  < scr-analysis.R >> $LOGS 2>&1
+
+# IRC
+echo "In IRC Analysis ..."
+LANG= R_LIBS=../../r-lib:$R_LIBS R --vanilla --args -r $REPORTS -d $7 -u root -i $1  -s $START -e $END -o $DIR -g months  < irc-analysis.R >> $LOGS 2>&1
