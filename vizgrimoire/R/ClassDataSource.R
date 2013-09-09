bf656156385d8cf6e42678bcd335f781349401d9
Daniel Izquierdo <dizquierdo@bitergia.com>
2013-09-09 12:58:17 +0200

    minor fixes

--- vizgrimoire/R/ClassDataSource.R
+++ vizgrimoire/R/ClassDataSource.R
@@ -23,18 +23,83 @@
 ## Data Source class
 ##
 
+
 setClass(
-    Class = "DataSource",
-    representation=representation(
-        type_analysis = "list",
-        startdate = "character",
-        enddate = "character",
-        evolutionary = "boolean"
-    ),
-    prototype = prototype(
-        type_analysis = list(NA, NA),
-        startdate = "1900-01-01",
-        enddate = "2100-01-01",
-        evolutionary = TRUE
-    )
+  Class = "DataSource",
+  representation=representation(
+    type_analysis = "list",
+    startdate = "character",
+    enddate = "character",
+    evolutionary = "numeric"
+  ),
+  prototype = prototype(
+    type_analysis = list(NA, NA),
+    startdate = "1900-01-01",
+    enddate = "2100-01-01",
+    evolutionary = 1 #TRUE=1, FALSE=0
+  )
 )
+
+
+setMethod(
+  "setAnalysis", "DataSource",
+  function(object, type, value) {
+    # type: company, repository, people, country
+    # value: any random string for each type
+    object@type_analysis = list(type, value)
+  }
+)
+
+setMethod(
+  "getAnalysis", "DataSource",
+  function(object) {
+    return (object@type_analysis)
+  }
+)
+
+
+
+setMethod(
+  "setStartDate", "DataSource",
+  function(object, startdate) {
+    # date with the format: yyyy-mm-dd
+    object@startdate = startdate
+  }
+)
+setMethod(
+  "getStartDate", "DataSource",
+  function(object) {
+    return (object@startdate)
+  }
+)
+
+
+setMethod(
+  "setEndDate", "DataSource",
+  function(object, enddate) {
+    # date with the format: yyyy-mm-dd
+    object@enddate = enddate
+  }
+)
+setMethod(
+  "getEndDate", "DataSource",
+  function(object) {
+    return (object@enddate)
+  }
+)
+
+
+setMethod(
+  "setEvolutionary", "DataSource",
+  function(object, evolutionary) {
+    # evolutionary = TRUE or FALSE
+    object@evolutionary = evolutionary
+  }
+)
+setMethod(
+  "getEvolutionary", "DataSource",
+  function(object) {
+    return (object@evolutionary)
+  }
+)
+
