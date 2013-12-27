c4068b0504cb5acd74e59b4359cace0b5be5a4c6
Alvaro del Castillo <acs@bitergia.com>
2013-12-27 12:24:17 +0100

    Added last activity data to IRC needed for tendencies.

--- vizgrimoire/R/IRC.R
+++ vizgrimoire/R/IRC.R
@@ -39,26 +39,26 @@ GetFiltersOwnUniqueIdsIRC <- function () {
 # GLOBAL
 
 GetStaticDataIRC <- function(period, startdate, enddate, i_db=NA, type_analysis=list(NA, NA)){
-    
+
     # 1- Retrieving information
     sent <- StaticNumSentIRC(period, startdate, enddate, i_db, type_analysis)
     senders <- StaticNumSendersIRC(period, startdate, enddate, i_db, type_analysis)
-    
+
     # 2- Merging information
     static_data = merge(sent, senders)
-    
+
     return (static_data)    
 }
 
 GetEvolDataIRC <- function(period, startdate, enddate, i_db=NA, type_analysis=list(NA, NA)){
-    
+
     # 1- Retrieving information
     sent <- EvolSentIRC(period, startdate, enddate, i_db, type_analysis)
     senders <- EvolSendersIRC(period, startdate, enddate, i_db, type_analysis)
-    
+
     # 2- Merging information
     evol_data = merge(sent, senders, all = TRUE)
-    
+
     return (evol_data)
 }
 
@@ -82,44 +82,6 @@ StaticNumSendersIRC <- function(period, startdate, enddate, identities_db=NA, ty
     return(ExecuteQuery(q))
 }
 
-GetIRCDiffMessagesDays <- function(period, init_date, days){
-    # This function provides the percentage in activity between two periods.
-    #
-    # The netvalue indicates if this is an increment (positive value) or decrement (negative value)
-
-    chardates = GetDates(init_date, days)
-    lastmessages = StaticNumSentIRC(period, chardates[2], chardates[1])
-    lastmessages = as.numeric(lastmessages[1])
-    prevmessages = StaticNumSentIRC(period, chardates[3], chardates[2])
-    prevmessages = as.numeric(prevmessages[1])
-    diffmessagesdays = data.frame(diff_netmessages = numeric(1), percentage_messages = numeric(1))
-
-    diffmessagesdays$diff_netmessages = lastmessages - prevmessages
-    diffmessagesdays$percentage_messages = GetPercentageDiff(prevmessages, lastmessages)
-
-    colnames(diffmessagesdays) <- c(paste("diff_netsent","_",days, sep=""), paste("percentage_sent","_",days, sep=""))
-
-    return (diffmessagesdays)
-}
-
-
-GetIRCDiffSendersDays <- function(period, init_date, identities_db=NA, days){
-    # This function provides the percentage in activity between two periods:
-    # Fixme: equal to GetDiffAuthorsDays
-
-    chardates = GetDates(init_date, days)
-    lastsenders = StaticNumSendersIRC(period, chardates[2], chardates[1], identities_db)
-    lastsenders = as.numeric(lastsenders[1])
-    prevsenders = StaticNumSendersIRC(period, chardates[3], chardates[2], identities_db)
-    prevsenders = as.numeric(prevsenders[1])
-    diffsendersdays = data.frame(diff_netsenders = numeric(1), percentage_senders = numeric(1))
-    diffsendersdays$diff_netsenders = lastsenders - prevsenders
-    diffsendersdays$percentage_senders = GetPercentageDiff(prevsenders, lastsenders)
-
-    colnames(diffsendersdays) <- c(paste("diff_netsenders","_",days, sep=""), paste("percentage_senders","_",days, sep=""))
-
-    return (diffsendersdays)
-}
 
 
 GetSentIRC <- function(period, startdate, enddate, identities_db, type_analysis, evolutionary){    
@@ -212,7 +174,7 @@ GetRepoEvolSentSendersIRC <- function(repo, period, startdate, enddate){
 
 GetRepoStaticSentSendersIRC <- function (repo, startdate, enddate) {
     fields = 'COUNT(irclog.id) AS sent, 
-              COUNT(DISTINCT(pup.upeople_id)) AS senders'    
+              COUNT(DISTINCT(pup.upeople_id)) AS senders'
     tables = GetTablesReposIRC()
     filters = paste(GetFiltersReposIRC()," AND c.name='",repo,"'",sep="")
     q <- GetSQLGlobal('date',fields, tables, filters, startdate, enddate)
@@ -265,4 +227,76 @@ GetStaticPeopleIRC <- function(developer_id, startdate, enddate) {
     query <- new("Query", sql = q)
     data <- run(query)
     return (data)
+}
+
+###############
+# Last Activity
+###############
+# The same as MLS.R lastActivity
+lastActivityIRC <- function(init_date, days) {
+    #commits
+    q <- paste("select count(distinct(id)) as sent_",days,"
+                from irclog
+                where date >= (", init_date, " - INTERVAL ",days," day)",
+               sep="")
+
+    query <- new("Query", sql = q)
+    data1 = run(query)
+    q <- paste("select count(distinct(pup.upeople_id)) as senders_",days,"
+                from irclog, people_upeople pup
+                where pup.people_id = nick  and
+                  date >= (", init_date, " - INTERVAL ",days," day)",
+               sep="")
+
+    query <- new("Query", sql = q)
+    data2 = run(query)
+
+    agg_data = merge(data1, data2)
+
+    return(agg_data)
+}
+
+
+
+##############
+# Microstudies
+##############
+
+GetIRCDiffSentDays <- function(period, init_date, days){
+    # This function provides the percentage in activity between two periods.
+    #
+    # The netvalue indicates if this is an increment (positive value) or decrement (negative value)
+
+    chardates = GetDates(init_date, days)
+    lastmessages = StaticNumSentIRC(period, chardates[2], chardates[1])
+    lastmessages = as.numeric(lastmessages[1])
+    prevmessages = StaticNumSentIRC(period, chardates[3], chardates[2])
+    prevmessages = as.numeric(prevmessages[1])
+    diffmessagesdays = data.frame(diff_netmessages = numeric(1), percentage_messages = numeric(1))
+
+    diffmessagesdays$diff_netmessages = lastmessages - prevmessages
+    diffmessagesdays$percentage_messages = GetPercentageDiff(prevmessages, lastmessages)
+
+    colnames(diffmessagesdays) <- c(paste("diff_netsent","_",days, sep=""), paste("percentage_sent","_",days, sep=""))
+
+    return (diffmessagesdays)
+}
+
+
+GetIRCDiffSendersDays <- function(period, init_date, identities_db=NA, days){
+    # This function provides the percentage in activity between two periods:
+    # Fixme: equal to GetDiffAuthorsDays
+
+    chardates = GetDates(init_date, days)
+    lastsenders = StaticNumSendersIRC(period, chardates[2], chardates[1], identities_db)
+    lastsenders = as.numeric(lastsenders[1])
+    prevsenders = StaticNumSendersIRC(period, chardates[3], chardates[2], identities_db)
+    prevsenders = as.numeric(prevsenders[1])
+    diffsendersdays = data.frame(diff_netsenders = numeric(1), percentage_senders = numeric(1))
+    diffsendersdays$diff_netsenders = lastsenders - prevsenders
+    diffsendersdays$percentage_senders = GetPercentageDiff(prevsenders, lastsenders)
+
+    colnames(diffsendersdays) <- c(paste("diff_netsenders","_",days, sep=""), paste("percentage_senders","_",days, sep=""))
+
+    return (diffsendersdays)
 }
