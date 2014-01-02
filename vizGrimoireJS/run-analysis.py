3556fdcde604454697fa5f4d67fe8566e14f58f5
Alvaro del Castillo <acs@bitergia.com>
2014-01-02 13:19:31 +0100

    Updated to laste version compatible with automator 0.3

--- vizGrimoireJS/run-analysis.py
+++ vizGrimoireJS/run-analysis.py
@@ -42,8 +42,13 @@ def get_options():
                           description='Executes the R scripts which analyze the information stored in the relational data bases and produce JSON files',
                           version='0.1')
 
-    parser.add_option('-f', dest='config_file', 
+    parser.add_option('-f', dest='config_file',
                       help='File path with the configuration for the R scripts',
+                      default=None)
+    parser.add_option('-g', '--debug', action='store_true', dest='debug',
+                        help='Enable debug mode', default=False)
+    parser.add_option('-s', dest='section',
+                      help='Section to be done: scm, its, mls, scr, irc, mediawiki, people',
                       default=None)
 
     (ops, args) = parser.parse_args()
@@ -76,7 +81,7 @@ def check_configuration():
     if 'db_bicho' in options['generic']:
         try:
             'bicho_backend' in options['generic'] == True
-        except:               
+        except:
             print "Configuration error: Configuration section for [generic] with 'backend_bicho'\
  variable expected"
             sys.exit(-1)
@@ -92,104 +97,108 @@ def get_vars():
     # if end_date is not present or is empty we set up today's date
     if not ('end_date' in v):
         v['end_date'] = time.strftime('%Y-%m-%d')
-        
+
     # FIXME this should be included in the main log file
     v['log_file'] = 'run-analysis.log'
-    if [v['db_password'] == ""]: v['db_password']="''"
+    if (v['db_password'] == ""):
+        v['db_password'] = "''"
     return v
 
-def execute_scm_script(myvars):
-    if not 'db_cvsanaly' in myvars:
+def get_analysis_cmd(v, script, db):
+    cmd = "LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s < %s >> %s 2>&1" % \
+              (v['r_libs'], v['reports'], db , v['db_user'], v['db_password'], \
+               v['db_identities'], v['start_date'], v['end_date'], v['json_dir'], \
+               v['period'], script, v['log_file'])
+    if (get_options().debug): print(cmd)
+    return (cmd)
+
+def execute_scm_script(env):
+    if not 'db_cvsanaly' in env:
         print("SCM analysis disabled")
         return
-    v = myvars
     print("Starting SCM analysis ..")
-    os.system("LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s < scm-analysis.R >> %s 2>&1" %
-              (v['r_libs'], v['reports'], v['db_cvsanaly'], v['db_user'],
-               v['db_password'], v['db_identities'], v['start_date'],
-               v['end_date'], v['json_dir'], v['period'], v['log_file']))
-    
-
+    cmd = get_analysis_cmd(env, "scm-analysis.R", env['db_cvsanaly'])
+    os.system(cmd)
     print("SCM analysis finished")
 
-def execute_its_script(myvars):
-    if not 'db_bicho' in myvars:
+def execute_people_script(env):
+    # TODO: right now people script uses cvsanaly db 
+    if not 'db_cvsanaly' in env:
+        print("People analysis disabled")
+        return
+    print("Starting People analysis ..")
+    cmd = get_analysis_cmd(env, "people-analysis.R", env['db_cvsanaly'])
+    os.system(cmd)
+    print("People analysis finished")
+
+
+def execute_its_script(env):
+    if not 'db_bicho' in env:
         print("ITS analysis disabled")
         return
-    v = myvars
     print("Starting ITS analysis  ..")
-    os.system("LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s -t %s < its-analysis.R >> %s 2>&1" %
-              (v['r_libs'], v['reports'], v['db_bicho'], v['db_user'],
-               v['db_password'], v['db_identities'], v['start_date'],
-               v['end_date'], v['json_dir'], v['period'], v['bicho_backend'],
-               v['log_file']))    
+    cmd = get_analysis_cmd(env, "its-analysis.R", env['db_bicho'])
+    os.system(cmd)
     print("ITS analysis finished")
 
-def execute_mls_script(myvars):
-    if not 'db_mlstats' in myvars:
+def execute_mls_script(env):
+    if not 'db_mlstats' in env:
         print("MLS analysis disabled")
         return
-    v = myvars
     print("Starting MLS analysis  ..")
-    os.system("LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s < mls-analysis.R >> %s 2>&1" %
-              (v['r_libs'], v['reports'], v['db_mlstats'], v['db_user'],
-               v['db_password'], v['db_identities'], v['start_date'], v['end_date'],
-               v['json_dir'], v['period'], v['log_file']))    
+    cmd = get_analysis_cmd(env, "mls-analysis.R", env['db_mlstats'])
+    os.system(cmd)
     print("MLS analysis finished")
 
-    
-def execute_scr_script(myvars):
-    if not 'db_gerrit' in myvars:
+def execute_scr_script(env):
+    if not 'db_gerrit' in env:
         print("SRC analysis disabled")
         return
-    v = myvars
     print("Starting SCR analysis  ..")
-    os.system("LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s < scr-analysis.R >> %s 2>&1" %
-              (v['r_libs'], v['reports'],v['db_gerrit'],v['db_user'],v['db_password'],
-               v['db_identities'],v['start_date'],v['end_date'],v['json_dir'],
-               v['period'],v['log_file']))    
+    cmd = get_analysis_cmd(env, "scr-analysis.R", env['db_gerrit'])
+    os.system(cmd)
     print("SCR analysis finished")
-    
 
-def execute_irc_script(myvars):
-    if not 'db_irc' in myvars:
+def execute_irc_script(env):
+    if not 'db_irc' in env:
         print("IRC analysis disabled")
         return
-    v = myvars
     print("Starting IRC analysis  ..")
-    os.system("LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s < irc-analysis.R >> %s 2>&1" %
-              (v['r_libs'], v['reports'],v['db_irc'],v['db_user'],v['db_password'],
-               v['db_identities'],v['start_date'],v['end_date'],v['json_dir'],
-               v['period'],v['log_file']))
+    cmd = get_analysis_cmd(env, "irc-analysis.R", env['db_irc'])
+    os.system(cmd)
     print("IRC analysis finished")
-    
-def execute_mediawiki_script(myvars):
-    if not 'db_mediawiki' in myvars:
+
+
+def execute_mediawiki_script(env):
+    if not 'db_mediawiki' in env:
         print("mediawiki analysis disabled")
         return
-    v = myvars
     print("Starting MediaWiki analysis  ..")
-    print("LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s < mediawiki-analysis.R >> %s 2>&1" %
-              (v['r_libs'], v['reports'],v['db_mediawiki'],v['db_user'],v['db_password'],
-               v['db_identities'],v['start_date'],v['end_date'],v['json_dir'],
-               v['period'],v['log_file']))
-
-    os.system("LANG= R_LIBS=%s R --vanilla --args -r %s -d %s -u %s -p %s -i %s -s %s -e %s -o %s -g %s < mediawiki-analysis.R >> %s 2>&1" %
-              (v['r_libs'], v['reports'],v['db_mediawiki'],v['db_user'],v['db_password'],
-               v['db_identities'],v['start_date'],v['end_date'],v['json_dir'],
-               v['period'],v['log_file']))
+    cmd = get_analysis_cmd(env, "mediawiki-analysis.R", env['db_mediawiki'])
+    os.system(cmd)
     print("MediaWiki analysis finished")
+
+tasks_section = {
+    'scm':execute_scm_script,
+    'people':execute_people_script,
+    'its':execute_its_script,
+    'mls':execute_mls_script,
+    'scr':execute_scr_script,
+    'mediawiki':execute_mediawiki_script,
+    'irc': execute_irc_script,
+}
+tasks_order = ['scm','people','its','mls','scr','mediawiki','irc']
+
+
 
 if __name__ == '__main__':
     opt = get_options()
     read_main_conf(opt.config_file)
     check_configuration()
-    myvars = {}
-    myvars = get_vars()
+    env = get_vars()
 
-    execute_mediawiki_script(myvars)
-    execute_scm_script(myvars)
-    execute_its_script(myvars)
-    execute_mls_script(myvars)
-    execute_scr_script(myvars)
-    execute_irc_script(myvars)
+    if opt.section is not None:
+        tasks_section[opt.section](env)
+    else:
+        for section in tasks_order:
+            tasks_section[section](env)
