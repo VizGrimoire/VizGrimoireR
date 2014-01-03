13d6350b70d19e02116b4a492f6ca1aa04d110a3
Alvaro del Castillo <acs@bitergia.com>
2014-01-03 08:59:10 +0100

    Added analysis people file.

---
+++ vizGrimoireJS/people-analysis.R
@@ -0,0 +1,64 @@
+## Copyright (C) 2012, 2013 Bitergia
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, write to the Free Software
+## Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+##
+## This file is a part of the vizGrimoire.R package
+##
+## Authors:
+##   Alvaro del Castillo <acs@bitergia.com>
+##
+##
+## Usage:
+##  R --vanilla --args -d dbname < people-analysis.R
+
+library("vizgrimoire")
+library("ISOweek")
+options(stringsAsFactors = FALSE) # avoid merge factors for toJSON 
+
+
+conf <- ConfFromOptParse()
+SetDBChannel (database = conf$database, user = conf$dbuser, password = conf$dbpassword)
+
+if (conf$granularity == 'years') { 
+    period = 'year'
+    nperiod = 365
+} else if (conf$granularity == 'months') { 
+    period = 'month'
+    nperiod = 31
+} else if (conf$granularity == 'weeks') { 
+    period = 'week'
+    nperiod = 7
+} else if (conf$granularity == 'days'){ 
+    period = 'day'
+    nperiod = 1
+} else {stop(paste("Incorrect period:",conf$granularity))}
+
+# destination directory
+destdir <- conf$destination
+
+people_data <- list()
+
+people  <- GetPeopleListSCM(conf$startdate, conf$enddate)
+people = people$pid
+limit = 100
+if (length(people)<limit) limit = length(people);
+people = people[1:limit]
+
+for (upeople_id in people) {
+    data <- GetPersonIdentifiers(upeople_id);
+    people_data[[as.character(upeople_id)]] <- GetPersonIdentifiers(upeople_id);
+}
+
+createJSON(people_data, paste(destdir,"/people.json", sep=''))
