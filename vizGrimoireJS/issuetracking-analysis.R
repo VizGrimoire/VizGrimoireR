05b7b71adffaaf7179f9bc413e00f25aba2219a5
Daniel Izquierdo <dizquierdo@bitergia.com>
2014-01-09 18:31:06 +0100

    Removing failing function from client

--- vizGrimoireJS/issuetracking-analysis.R
+++ vizGrimoireJS/issuetracking-analysis.R
@@ -134,12 +134,12 @@ for (status in statuses)
     colnames(tickets_status)[2] <- status
 
     #Issues per status
-    current_status <- GetCurrentStatus(period, startdate, enddate, identities_db, status)
+    #current_status <- GetCurrentStatus(period, startdate, enddate, identities_db, status)
     
     #Merging data
-    if (nrow(current_status)>0){
-        evol <- merge(evol, current_status, all=TRUE)
-    }
+    #if (nrow(current_status)>0){
+    #    evol <- merge(evol, current_status, all=TRUE)
+    #}
     evol <- merge (evol, tickets_status, all = TRUE)
 }
 
