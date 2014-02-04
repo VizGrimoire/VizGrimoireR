ce2856f1e3a8ee4f77dd89b74264d5216f7645e2
Alvaro del Castillo <acs@bitergia.com>
2014-02-04 10:48:29 +0100

    Fix the repo field for MLS lists.

--- vizGrimoireJS/mls-analysis.R
+++ vizGrimoireJS/mls-analysis.R
@@ -53,6 +53,7 @@ enddate <- conf$enddate
 #
 options(stringsAsFactors = FALSE) # avoid merge factors for toJSON 
 rfield = reposField()
+rfield = 'mailing_list_url'
 
 data <- EvolMLSInfo(period, startdate, enddate, identities_db, rfield)
 
