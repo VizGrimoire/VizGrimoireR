ce44e2b03286d57ab6ac48773a344dcbf40bcc63
Daniel Izquierdo <dizquierdo@bitergia.com>
2014-02-25 15:35:13 +0100

    Constructor ready

---
+++ vizGrimoireJS/mls-studies.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2014 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# This file is a part of the vizGrimoire package
+#
+## Authors:
+## Daniel Izquierdo Cortazar <dizquierdo@bitergia.com>
+
+import logging
+import sys
+import GrimoireUtils, GrimoireSQL
+from GrimoireUtils import dataFrame2Dict, createJSON, completePeriodIds
+from optparse import OptionParser
+from mlstopics import MLSTopics
+
+def read_options():
+    parser = OptionParser(usage="usage: %prog [options]",
+                          version="%prog 0.1")
+    parser.add_option("-u","--dbuser",
+                      action="store",
+                      dest="dbuser",
+                      default="root",
+                      help="Database user")
+    parser.add_option("-p","--dbpassword",
+                      action="store",
+                      dest="dbpassword",
+                      default="",
+                      help="Database password")
+    parser.add_option("-m", "--mls",
+                      action="store",
+                      dest="dbmls",
+                      help="MLS database")
+    parser.add_option("-t", "--identities",
+                      action="store",
+                      dest="dbidentities",
+                      help="Identities database")
+    (opts, args) = parser.parse_args()
+
+    if len(args) != 0:
+        parser.error("Wrong number of arguments")
+
+    if not(opts.dbidentities and opts.dbuser):
+        parser.error("--dbuser and --dbidentities are needed")
+    return opts
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s')
+    logging.info("Starting Studies analysis")
+    opts = read_options()
+   
+    GrimoireSQL.SetDBChannel (database=opts.dbmls, user=opts.dbuser, password=opts.dbpassword)
+    main_topics = MLSTopics('2014-01-01')
+
--- vizgrimoire/analysis/mlstopics.py
+++ vizgrimoire/analysis/mlstopics.py
@@ -18,25 +18,78 @@
 #
 #
 # Authors:
-#     Daniel Izquierdo Cortázar <dizquierdo@bitergia.com>
+#     Daniel Izquierdo Cortazar <dizquierdo@bitergia.com>
 
 # Mailing lists main topics study
 
-class mls_topics(object):
+import GrimoireUtils
+import GrimoireSQL
+from GrimoireSQL import ExecuteQuery
+
+class MLSTopics(object):
     # This class contains the analysis of the mailing list from the point
     # of view of threads. The main topics are those with the longest,
     # the most crowded or the thread with the most verbose emails.
 
-    def __init__ (self, date)
+    def __init__ (self, date):
         self.date = date
         # Initialize data structure
-        self.threads = _init_threads()
+        self.list_message_id = []
+        self.list_is_response_of = []
+        self.threads = {}
+        self._init_threads()
         self.crowded = None # the thread with most people participating
         self.longest = None # the thread with the longest queue of emails
         self.verbose = None # the thread with the most verbose emails.
 
+    
+
+    def _build_threads (self, message_id):
+        sons = []
+        messages = []
+        if message_id not in self.list_is_response_of:
+            # this a leaf of the tree!
+            print "Leaf of the tree: message_id = " + message_id
+            return [message_id]
+
+        else:
+            cont = 0
+            for msg in self.list_is_response_of:
+                #Retrieving all of the sons of a given msg
+                if msg == message_id:
+                    sons.append(self.list_message_id[cont])
+                cont = cont + 1
+            for msg in sons:
+                messages.extend(self._build_threads(msg))            
+
+        return messages          
+            
+
     def _init_threads(self):
-        
+        # Returns dictionary of message_id threads. Each key contains a list
+        # of emails associated to that thread (not ordered).
+        query = """
+                select message_ID, is_response_of from messages 
+                where is_response_of is not NULL
+                """
+        msg_couples = ExecuteQuery(query)
+        self.list_message_id = msg_couples["message_ID"]
+        self.list_is_response_of = msg_couples["is_response_of"]
+
+        messages = {}
+        for message_id in self.list_message_id:
+            # Looking for messages in the thread
+            messages[message_id] = self._build_threads(message_id)
+
+        query = """
+                select message_ID from messages where is_response_of is NULL
+                """
+        result = ExecuteQuery(query)
+        msgs = result["message_ID"]
+        for msg in msgs:
+            messages[msg] = []
+
+
 
     def crowded_list (self):
         # Returns the most crowded thread.
@@ -44,22 +97,25 @@ class mls_topics(object):
         # participants
         if self.crowded == None:
             # variable was not initialize
+            pass
 
     def longest_list (self):
         # Returns the longest thread
-        if self.longest == None;
+        if self.longest == None:
             # variable was not initialize
+            pass
 
     def verbose_list (self):
         # Returns the most verbose thread (the biggest emails)
         if self.verbose == None:
             # variable was not initialize
+            pass
 
     def threads (self):
         # Returns the whole data structure
         if self.threads == None:
             # variable was not initialize
-            self.threads = _init_threads()
+            pass
 
         return self.threads
 
